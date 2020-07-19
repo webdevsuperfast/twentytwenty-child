1a6266024e51cc75d9e10907f07cbcda07c27c25
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2020-07-19 09:44:44 +0800

    Reintroduce back to top functionality

--- footer.php
+++ footer.php
@@ -62,7 +62,20 @@ $footer_classes .= $has_footer_menu ? ' has-footer-menu' : '';
 				</div><!-- .section-inner -->
 
 			</footer><!-- #site-footer -->
-
+			<a class="to-the-top" href="#site-header">
+				<span class="to-the-top-long">
+					<?php
+					/* translators: %s: HTML character for up arrow. */
+					printf( __( 'To the top %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
+					?>
+				</span><!-- .to-the-top-long -->
+				<span class="to-the-top-short">
+					<?php
+					/* translators: %s: HTML character for up arrow. */
+					printf( __( 'Up %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
+					?>
+				</span><!-- .to-the-top-short -->
+			</a><!-- .to-the-top -->
 		<?php wp_footer(); ?>
 
 	</body>
